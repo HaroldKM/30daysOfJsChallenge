9805176bcb42164f14ab936413732eb08c58a245
HaroldKM <harodasus1230@gmail.com>
2022-06-19 08:32:33 -0500

    4-casting and rounding up

--- d02/level2.js
+++ d02/level2.js
@@ -2,4 +2,17 @@
 console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
 
 // 2 
-console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.");
+console.log("\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"")
+
+
+// 3 casting -->  str to int 
+let str10 = '10'
+console.log(`${str10 === 10}`)
+let int10 = +'10'
+console.log(`${int10 === 10}`);
+
+// 4 casting and rounding up 
+let float10 = parseFloat("9.8")
+console.log(`${float10 == 10}`);
+let roundedFloat10 = Math.ceil(float10)
+console.log(`${roundedFloat10 == 10}`);
