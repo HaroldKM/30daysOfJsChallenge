963ee046038b265f1735ef22d66e1a2609e02230
HaroldKM <harodasus1230@gmail.com>
2022-10-06 17:05:09 +0100

    d08 | level1 DONE

--- d08/level1.js
+++ d08/level1.js
@@ -5,23 +5,28 @@ const dog = {};
 console.log(dog);
 
 // 3 -Add name, legs, color, age and bark properties for the dog object
-(dog.name = "zeus"),
-  (dog.legs = 4),
-  (dog.color = "black-brown"),
-  (dog.bark = () => console.log("woof, woof"));
+dog.name = "zeus"
+dog.legs = 4
+dog.color = "black-brown"
+dog.bark = () => console.log("woof, woof");
 
-// 4 get all values from object dog
+// 4 get all values from any object 
 const getObjectValue = (obj) => {
-  console.log("heko");
   const objKeys = Object.keys(obj);
   for (let i = 0; i < objKeys.length; i++) {
     let value = obj[objKeys[i]];
     if (typeof value === "function") {
-      value();
+      value(); // if amoung the obj element there is a function, call it
     } else {
       console.log(`${objKeys[i]}: ${value}`);
     }
   }
 };
+
+getObjectValue(dog);
+
+// 5 set new property
+dog.breed = "i'm bredding"
+// for dogInfo just call getObjectValue(dog)
 
 getObjectValue(dog);
